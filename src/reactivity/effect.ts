80e7d27ddc6ee3966d79ef0b0eca68dabc58340a
cl1997 <Leiloloaa@gmail.com>
2021-12-13 13:46:55 +0800

    修补 reactivity

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -17,17 +17,20 @@ export class ReactiveEffect {
     // 如果 active 为 false 
     // 那么 就已经 stop 了
     // 否则就应该收集 activeEffect
+    // 如果是 stop 状态 就只执行 不赋值给 activeEffect
     if (!this.active) {
       return this._fn()
     }
     shouldTrack = true
     activeEffect = this
+    // 执行 fn 的时候，就会 get 操作 就会收集
     const r = this._fn()
 
     // todo 为什么要重置？
     // 第一次收集完后，就不用再收集了
     // 重置
     shouldTrack = false
+    activeEffect = undefined
     return r
   }
   stop() {
@@ -54,6 +57,7 @@ function cleanupEffect(effect) {
 
 const targetsMap = new Map()
 export function track(target, key) {
+  // 是否收集  shouldTrack 为 true 和 activeEffect 有值的时候要收集 否则就 return 出去
   if (!isTracking()) return;
   // 收集依赖
   // reactive 传入的是一个对象 {}
@@ -76,15 +80,15 @@ export function track(target, key) {
   if (!activeEffect) return
   // if (!shouldTrack) return
 
-  // // 如果 dep 存在 就不会收集
+  // 如果 dep 存在 就不会收集
   // if (dep.has(activeEffect)) return
 
-  // // 要存入的是一个 fn
-  // // 所以要利用一个全局变量
+  // 要存入的是一个 fn
+  // 所以要利用一个全局变量
   // dep.add(activeEffect)
 
-  // // 如何通过当前的 effect 去找到 deps？
-  // // 反向收集 deps
+  // 如何通过当前的 effect 去找到 deps？
+  // 反向收集 deps
   // activeEffect.deps.push(dep)
   trackEffects(dep)
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,21 +1,31 @@
 import { isObject } from './../shared/index';
 import { mutableHandlers, readonlyHandlers, shallowReadonlyHandlers } from "./baseHandler"
 
+// 创建了 3 个 WeakMap
+export const reactiveMap = new WeakMap();
+export const readonlyMap = new WeakMap();
+export const shallowReadonlyMap = new WeakMap();
+
 export const enum ReactiveFlags {
   IS_REACTIVE = "__v_isReactive",
   IS_READONLY = "__v_isReadonly",
+  RAW = "__v_raw",
 }
 
-export function reactive(raw) {
-  return createReactiveObject(raw, mutableHandlers)
+export function reactive(target) {
+  return createReactiveObject(target, reactiveMap, mutableHandlers);
 }
 
-export function readonly(raw) {
-  return createReactiveObject(raw, readonlyHandlers)
+export function readonly(target) {
+  return createReactiveObject(target, readonlyMap, readonlyHandlers);
 }
 
-export function shallowReadonly(raw) {
-  return createReactiveObject(raw, shallowReadonlyHandlers)
+export function shallowReadonly(target) {
+  return createReactiveObject(
+    target,
+    shallowReadonlyMap,
+    shallowReadonlyHandlers
+  );
 }
 
 export function isReactive(value) {
@@ -33,10 +43,39 @@ export function isProxy(value) {
 
 // 抽离 new proxy 生成createReactiveObject
 // target = raw ；baseHandles 类别
-function createReactiveObject(target, baseHandles) {
+function createReactiveObject(target, proxyMap, baseHandles) {
+  // proxyMap 的目的是为确定是否存在过
+
+  // 如果命中的话就直接返回就好了
+  // 使用缓存做的优化点
+  const existingProxy = proxyMap.get(target);
+  if (existingProxy) {
+    return existingProxy;
+  }
+
   if (!isObject(target)) {
     console.log('必须是一个对象')
     return target
   }
-  return new Proxy(target, baseHandles)
+
+  const proxy = new Proxy(target, baseHandles)
+
+  // 并且把创建好的保存起来
+  proxyMap.set(target, proxy)
+
+  return proxy
+}
+
+export function toRaw(value) {
+  // 如果 value 是proxy 的话 ,那么直接返回就可以了
+  // 因为会触发 createGetter 内的逻辑
+  // 如果 value 是普通对象的话，
+  // 我们就应该返回普通对象
+  // 只要不是 proxy ，只要是得到的 undefined 的话，那么就一定是普通对象
+  // TODO 这里和源码里面实现的不一样，不确定后面会不会有问题
+  if (!value[ReactiveFlags.RAW]) {
+    return value;
+  }
+
+  return value[ReactiveFlags.RAW];
 }
