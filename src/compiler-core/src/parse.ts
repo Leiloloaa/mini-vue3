02ae6ac54d5f6cda029ab8bf0cf2441ec129b376
cl1997 <Leiloloaa@gmail.com>
2022-02-22 15:06:49 +0800

    实现解析 element 标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content) {
   const context = createParserContent(content)
 
@@ -9,13 +14,44 @@ export function baseParse(content) {
 function parseChildren(context) {
   const nodes: any = []
   let node
-  if (context.source.startsWith('{{')) {
+  const s = context.source;
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context)
+  } else if (/^<[a-z]*/i.test(s)) {
+    // 需要用正则表达判断
+    // <div></div>
+    // /^<[a-z]/i/
+    node = parseElement(context);
   }
 
   nodes.push(node)
 
   return nodes
+}
+
+function parseElement(context) {
+  // 解析标签
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+
+  return element
+}
+
+function parseTag(context, type) {
+  // <div></div>
+  // 匹配解析
+  // 推进
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  // 获取完后要推进
+  advanceBy(context, match[0].length + 1)
+
+  if (type === TagType.End) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
 }
 
 function parseInterpolation(context) {
@@ -32,7 +68,8 @@ function parseInterpolation(context) {
   )
 
   // 删除 前两个字符串
-  context.source = context.source.slice(openDelimiter.length)
+  // context.source = context.source.slice(openDelimiter.length)
+  advanceBy(context, openDelimiter.length)
 
   // 内容的长度就等于 closeIndex - openDelimiter 的长度
   const rawContentLength = closeIndex - openDelimiter.length
@@ -40,7 +77,8 @@ function parseInterpolation(context) {
   const content = rawContent.trim()
 
   // 然后还需要把这个字符串给删了 模板是一个字符串 要接着遍历后面的内容
-  context.source = context.source.slice(rawContentLength + closeDelimiter.length);
+  // context.source = context.source.slice(rawContentLength + closeDelimiter.length);
+  advanceBy(context, rawContentLength + closeDelimiter.length)
 
   return {
     type: NodeTypes.INTERPOLATION,
@@ -49,6 +87,10 @@ function parseInterpolation(context) {
       content: content
     }
   }
+}
+
+function advanceBy(context, length) {
+  context.source = context.source.slice(length);
 }
 
 function createRoot(children) {
